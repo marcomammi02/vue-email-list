31d0f31b33f29f38eafa6b927bad1be7174640bd
marcomammi02 <marcomammi2002@gmail.com>
2022-11-23 10:16:42 +0100

    Add new emails function

--- script.js
+++ script.js
@@ -6,15 +6,14 @@ new Vue({
     },
     methods: {
         getRandomEmail() {
-            if (this.arrEmails.length < 10) {
-                for (let i = 0; i < 10; i++) {
-                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                        .then(axiosResponse => {
-                            console.log(axiosResponse);
-                            this.randomEmail = axiosResponse.data.response;
-                            this.arrEmails.push(this.randomEmail);
-                        });
-                };
+            this.arrEmails = [];
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then(axiosResponse => {
+                        console.log(axiosResponse);
+                        this.randomEmail = axiosResponse.data.response;
+                        this.arrEmails.push(this.randomEmail);
+                    });
             };
         },
     },
